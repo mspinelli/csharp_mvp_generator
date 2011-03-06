cab58e8d2a5b559b5992dfd450089e1124d430b0
unknown <mspinelli@.(none)>
2011-03-05 19:11:30 -0500

    Checked in RubyMine project files

--- code_generator/lib/erb_binding.rb
+++ code_generator/lib/erb_binding.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/ruby
 #See this link: http://www.paracode.com/blog/2011/01/248/
 #It seems to allow the code generator to work when binding to the erb
-#What happens is this: http://stackoverflow.com/questions/3242470/problem-using-openstruct-with-erb
+#What happens is as follows:
+#http://stackoverflow.com/questions/3242470/problem-using-openstruct-with-erb
 require 'ostruct'
 
 class ErbBinding < OpenStruct
