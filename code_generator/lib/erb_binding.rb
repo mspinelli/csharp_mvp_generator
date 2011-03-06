b20addc973c64cae96953607a9c2769a7a86ba70
Matt Spinelli <mspinelli.software@gmail.com>
2011-03-05 19:01:27 -0500

    Checked in RubyMine project files Added erb_binding.rb

--- code_generator/lib/erb_binding.rb
+++ code_generator/lib/erb_binding.rb
@@ -1,3 +1,11 @@
-class ErbBinding
-  # To change this template use File | Settings | File Templates.
+#!/usr/bin/ruby
+#See this link: http://www.paracode.com/blog/2011/01/248/
+#It seems to allow the code generator to work when binding to the erb
+#What happens is this: http://stackoverflow.com/questions/3242470/problem-using-openstruct-with-erb
+require 'ostruct'
+
+class ErbBinding < OpenStruct
+  def get_binding
+    binding()
+  end
 end
